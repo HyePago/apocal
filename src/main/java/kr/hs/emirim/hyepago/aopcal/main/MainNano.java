93b4dbb7abb0a044966c53fa0cbf5b79e2251852
Hyejin Kim <hyepago@naver.com>
2018-06-20 14:05:30 +0900

    실행시간 측정하기 3 - 나노시간 측정

---
+++ src/main/java/kr/hs/emirim/hyepago/aopcal/main/MainNano.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.hyepago.aopcal.main;
+
+import kr.hs.emirim.hyepago.aopcal.cal.Calculator;
+import kr.hs.emirim.hyepago.aopcal.cal.ImpCalculator;
+import kr.hs.emirim.hyepago.aopcal.cal.RecCalculator;
+
+public class MainNano {
+
+	public static void main(String[] args) {
+		long num = 10000;
+		long start1 = System.nanoTime();
+		Calculator cal1 = new ImpCalculator();
+		long f1 = cal1.factorial(num);
+		long end1 = System.nanoTime();
+		System.out.printf("Imp factorial(%d): 실행시간 = %d", num, (end1 - start1));
+		//System.out.println("Imp 100 : " + f1);
+		System.out.println("");
+		
+		long start2 = System.nanoTime();
+		Calculator cal2 = new RecCalculator();
+		long f2 = cal2.factorial(num);
+		long end2 = System.nanoTime();
+		System.out.printf("Imp factorial(%d): 실행시간 = %d", num, (end2 - start2));
+		//System.out.println("Imp 100 : " + f2);
+	}
+
+}
