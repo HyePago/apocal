ef792999ce2de564f9cb9537b41266d833cc5f2e
Hyejin Kim <hyepago@naver.com>
2018-06-20 14:02:51 +0900

    실행시간 측정하기 2 - 메서드 실행 전후 시간 측정

--- src/main/java/kr/hs/emirim/hyepago/aopcal/cal/RecCalculator.java
+++ src/main/java/kr/hs/emirim/hyepago/aopcal/cal/RecCalculator.java
@@ -6,15 +6,10 @@ public class RecCalculator implements Calculator {
 	public long factorial(long num) {
 		long start = System.currentTimeMillis();
 		
-		try {
-			if (num == 0)
-				return 1;
-			else
-				return num * factorial(num - 1);
-		} finally {
-			long end = System.currentTimeMillis();
-			System.out.printf("Imp factorial(%d): 실행시간 = %d", num, (end - start));
-		}
+		if (num == 0)
+			return 1;
+		else
+			return num * factorial(num - 1);
 	}
 	
 }
